3540e696bb20cfc2c0b058fc24ad43d96b7d87ca
SAHAL MAGHFUD <107709509+sahalmaghfud@users.noreply.github.com>
2025-08-27 10:32:33 +0700

    tess

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -221,7 +221,7 @@
             // 1. Grafik Jenis Dokumen (Pie Chart)
             const jenisDokumenCtx = document.getElementById('jenisDokumenChart').getContext('2d');
             const jenisDokumenData = {
-                'Dokumen Masuk': 1423,
+                'Dokumen Masuk': 142,
                 'Dokumen Keluar': 76
             };
 
